85866549f8517f884a4889202789b6071c29f040
yekyawaung91 <yekyawaung1991@gmail.com>
2026-03-31 09:58:51 +0630

    fixed log viewer permission issue

--- config/log-viewer.php
+++ config/log-viewer.php
@@ -112,7 +112,8 @@ return [
 
     'api_middleware' => [
         \Opcodes\LogViewer\Http\Middleware\EnsureFrontendRequestsAreStateful::class,
-        \Opcodes\LogViewer\Http\Middleware\AuthorizeLogViewer::class,
+        \Opcodes\LogViewer\Http\Middleware\CustomAuthorizeLogViewer::class,
+        //\Opcodes\LogViewer\Http\Middleware\AuthorizeLogViewer::class,
     ],
 
     'api_stateful_domains' => env('LOG_VIEWER_API_STATEFUL_DOMAINS') ? explode(',', env('LOG_VIEWER_API_STATEFUL_DOMAINS')) : null,
